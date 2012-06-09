9b6db2f51b6ff9d79adc46ab21f1cdb218a4f059
mattortiz <matt@mattortiz.com>
2012-06-09 01:27:45 -0700

    .

--- AgileNinja/Global.asax.cs
+++ AgileNinja/Global.asax.cs
@@ -9,7 +9,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+ 
 namespace AgileNinja
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
